3464fd0e8167bb04153fba6de2879ba899772206
monta <monta@FLORIAN-HAUT.local>
2023-01-06 18:13:15 +0100

    Add init source

---
+++ main.py
@@ -0,0 +1,241 @@
+from datetime import datetime, timezone
+import pandas as pd
+from openpyxl.worksheet.worksheet import Worksheet
+from pandas import json_normalize
+import requests
+import feedparser
+from bs4 import BeautifulSoup
+import re
+import calendar
+import time
+import configparser
+from openpyxl import Workbook
+
+
+# Code form
+# https://medium.com/analytics-vidhya/web-scraping-news-data-rss-feeds-python-and-google-cloud-platform-7a0df2bafe44
+
+class news_feed_parser:
+    # Parse rss feed url
+    def parse_rss_feed(self, url):
+        # Read feed xml data
+        # Try 3 times requesting the url if error
+        for i in range(0, 4):
+            try:
+                news_feed = feedparser.parse(url)
+                break
+            except:
+                print('ERROR calling URL:', url, 'iter: ', (i + 1))
+                pass
+
+        # Flatten data
+        df_news_feed = json_normalize(news_feed.entries)
+
+        return df_news_feed
+
+    # Process articles dataset
+    def process_article(self, article_url, article_title):
+        # Loop to parse each rss feed article url
+        articles_dataset = pd.DataFrame(columns=['url', 'title', 'text'])
+        article_text = self.parse_article(article_url)
+        if article_text != None:
+            new_data = pd.DataFrame([{'url': article_url
+                                         , 'title': article_title
+                                         , 'text': article_text
+                                      }], columns=['url', 'title', 'text'])
+            articles_dataset = pd.concat((articles_dataset, new_data), ignore_index=True, sort=False)
+
+        articles_dataset['text_length'] = articles_dataset['text'].apply(lambda x: len(x))
+        return articles_dataset
+
+    # Build and return parent hierarchy
+    def get_parent_hierarchy(self, article_parents):
+        # Loop for each paragraph parent to extract its element name and id
+        parents_list = []
+        for parent in article_parents:
+            # Extract the parent id attribute if it exists
+            Parent_id = ''
+            try:
+                Parent_id = parent['id']
+            except:
+                pass
+
+            # Append the parent name and id to the parents table
+            parents_list.append(parent.name + 'id: ' + Parent_id)
+
+        # 2.2 Construct paragraph parent hierarchy
+        parent_element_list = ['' if (x == 'None' or x is None) else x for x in parents_list]
+        parent_element_list.reverse()
+        parent_hierarchy = ' -> '.join(parent_element_list)
+
+        return parent_hierarchy
+
+    # Parse url and return article text
+    def parse_article(self, article_url):
+        # Request the article url to get the web page content.
+        article_result = requests.get(article_url)
+
+        # 1. extract all paragraph elements inside the page body
+        article_content = BeautifulSoup(article_result.content, 'html.parser')
+        articles_body = article_content.findAll('body')
+
+        p_blocks = articles_body[0].findAll('p')
+
+        # 2. for each paragraph, construct its patents elements hierarchy
+        # Create a dataframe to collect p_blocks data
+        p_blocks_df = pd.DataFrame(columns=['element_name', 'parent_hierarchy', 'element_text', 'element_text_Count'])
+
+        # 2.1 loop for each paragraph block
+        article_text = ''
+        for i in range(0, len(p_blocks)):
+            # Get paragraph parent hierarchy
+            parent_hierarchy = self.get_parent_hierarchy(p_blocks[i].parents)
+
+            # Append p_blocks_df with the current paragraph data
+            new_block_data = pd.DataFrame([{"element_name": p_blocks[i].name
+                                               , "parent_hierarchy": parent_hierarchy
+                                               , "element_text": p_blocks[i].text
+                                               , "element_text_Count": len(str(p_blocks[i].text))
+                                            }])
+
+            p_blocks_df = pd.concat([p_blocks_df, new_block_data]
+                                    , ignore_index=True
+                                    , sort=False
+                                    )
+
+            # 3. concatenate paragraphs under the same parent hierarchy
+            if len(p_blocks_df) > 0:
+                # Group paragraphs by parent_hierarchy
+                p_blocks_df_groupby_parent_hierarchy = p_blocks_df.groupby(by=['parent_hierarchy'])
+
+                # Sum the paragraph lenght for each paragraph group
+                p_blocks_df_groupby_parent_hierarchy_sum = p_blocks_df_groupby_parent_hierarchy[
+                    ['element_text_Count']].sum()
+                p_blocks_df_groupby_parent_hierarchy_sum.reset_index(inplace=True)
+
+            # 4. select the longest paragraph as the main article
+            max_id = p_blocks_df_groupby_parent_hierarchy_sum.loc[
+                p_blocks_df_groupby_parent_hierarchy_sum['element_text_Count'].idxmax()
+                , 'parent_hierarchy']
+            article_text = '\n'.join(
+                p_blocks_df.loc[p_blocks_df['parent_hierarchy'] == max_id, 'element_text'].to_list())
+
+        # Return article text
+        return article_text
+
+
+class EmptyTextException(Exception):
+    """Raised when the article text is empty"""
+    pass
+
+
+def init_sheet():
+    """
+    Init the Workbook, a worksheet for the result and a worksheet for the error
+    :return: [Workbook, Worksheet, Worksheet]
+    """
+
+    # Create Workbook
+    workbook = Workbook()
+    ws_result: Worksheet = workbook.active
+    ws_result.title = "Results"
+    ws_error: Worksheet = workbook.create_sheet("Errors")
+
+    # Add Title to column in the first row for the Results sheet
+    ws_result['A1'] = 'URL'
+    ws_result['B1'] = 'Title'
+    ws_result['C1'] = 'Text'
+
+    # Add Title to column in the first row for the Error sheet
+    ws_error['A1'] = 'Original URL'
+    ws_error['B1'] = 'Working URL'
+    ws_error['C1'] = 'Reason'
+
+    return workbook, ws_result, ws_error
+
+
+def add_row(work_sheet, row_index, dataset):
+    working_url = dataset.url[0]
+    title = dataset.title[0]
+    text = dataset.text[0]
+
+    if not text:
+        raise EmptyTextException
+
+    work_sheet['A' + str(row_index)] = working_url
+    work_sheet['B' + str(row_index)] = title
+    work_sheet['C' + str(row_index)] = text
+
+
+def add_error_row(error_work_sheet, row_index, original_url, working_url, reason='Generic Error'):
+    error_work_sheet['A' + str(row_index)] = original_url
+    error_work_sheet['B' + str(row_index)] = working_url
+    error_work_sheet['C' + str(row_index)] = reason
+
+
+def get_config():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    return config
+
+
+def get_rss_feed_url():
+    return get_config()['GoogleAlert']['rss_feed_url']
+
+
+if __name__ == '__main__':
+    url = get_rss_feed_url()
+
+    # Try running the search-and-extract process
+    try:
+        # Get articles from news feed
+        my_rssFeed = news_feed_parser()
+        news_feed_data = my_rssFeed.parse_rss_feed(url)
+    except:
+        print("Error: calling news feed url: {} ".format(url))
+        raise
+
+    # Get number of items to work on
+    number_of_news = len(news_feed_data)
+    print('news_feed_data len: ', number_of_news)
+
+    wb, ws, ws_error = init_sheet()
+
+    # We use our own index instead of the for idx because if we have errors we will have
+    # blank row in the Result sheet
+    error_idx = 0
+    result_idx = 0
+    for idx, row in news_feed_data.iterrows():
+        print("Working on news #" + str(idx) + " of " + str(number_of_news))
+
+        try:
+            link = row.link
+            re_result = re.search("^.*url=(.*)&ct.*$", link)
+            true_link = re_result.group(1)
+
+            try:
+                dataset = my_rssFeed.process_article(true_link, row.title)
+                add_row(ws, result_idx + 2, dataset)
+                result_idx += 1
+            except IndexError:
+                add_error_row(ws_error, error_idx + 2, link, true_link)
+                error_idx += 1
+                print("Error while getting: {} ".format(link))
+                continue
+            except EmptyTextException:
+                reason_text = 'Empty article text'
+                add_error_row(ws_error, error_idx + 2, link, true_link, reason_text)
+                error_idx += 1
+                print("Error: {} while getting: {} ".format(reason_text, link))
+                continue
+
+        except:
+            print("Error: File Upload failed.")
+            continue
+
+    # Build file name
+    current_GMT = time.gmtime()
+    time_stamp = calendar.timegm(current_GMT)
+    filename = "results/result" + str(time_stamp) + ".xlsx"
+    wb.save(filename)
